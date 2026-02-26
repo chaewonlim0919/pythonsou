8e3f3501a9115b22e2488776b1f156b669bff194
chaewon <chaewonlim0919@naver.com>
2026-02-26 18:19:16 +0900

    2/26

---
+++ pro2network/pack3http/cgi-bin/sangpum.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-   
+# 한글 깨짐 현상 해결 - 위 명령 안 먹으면 아래 방법 사용
+import sys
+sys.stdout.reconfigure(encoding='utf-8')   
+
+import MySQLdb
+import pickle
+
+with open('cgi-bin/mydb.dat', mode='rb') as obj:
+    config= pickle.load(obj)
+
+print("Content-Type: text/html; charset=utf-8")
+print()
+print("<html><body><b>**상품정보**</b><br/>")
+print("<table border='1'>")      # table은 표로 출력, border은 열었으면 닫아
+print("<tr><td>코드</td><td>상품명</td><td>수량</td><td>단가</td></tr>")
+
+try:
+    conn = MySQLdb.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute("select * from sangdata order by code desc")
+    datas = cursor.fetchall()
+    for code, sang, su, dan in datas:
+        print("""
+            <tr>
+                <td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td>
+            </tr>
+        """.format(code, sang, su, dan))
+
+except Exception as e:
+    print('err: ', e)
+
+finally:
+    cursor.close()
+    conn.close()
+
+print("</table>")
+print("</body></html>")
